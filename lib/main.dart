1d79fe8216dfa3ae624ca455b4f0c371a557c6d0
Lensa <lensabillion67@gmail.com>
2021-06-30 14:45:42 +0300

    bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-     
     debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
@@ -12,15 +11,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: PreferredSize(
-  preferredSize: Size.fromHeight(200.0),
-  child: AppBar(
-    automaticallyImplyLeading: false, // hides leading widget
-    
-  )
-)
-);
-       
+          preferredSize: Size.fromHeight(200.0),
+          child: AppBar(
+            automaticallyImplyLeading: false, // hides leading widget
+          )
+          ),
+
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+           backgroundColor: Colors.blue,
+           items: [BottomNavigationBarItem(
+            title: Text('Favorites'),
+            icon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Music'),
+            icon: Icon(Icons.music_note),
+          ),],
+          ),
+    );
   }
 }
